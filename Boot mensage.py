abaa88469dfb563b2363ae5fe7a787e3e888c6c9
jms05 <a72023@alunos.umiho.pt>
2015-02-21 21:37:45 +0000

    Added All Scrips
    Added all scrips to make LCD working (I2C_16x2) with standard functions
    in the future more will come

---
+++ Boot mensage.py
@@ -0,0 +1,119 @@
+# requires RPi_I2C_driver.py
+import RPi_I2C_driver
+from time import *
+
+mylcd = RPi_I2C_driver.lcd()
+
+mylcd.lcd_display_string("      HELLO     ", 1)
+mylcd.lcd_display_string("               B", 2) 
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("             BOO", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("            BOOT", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("           BOOTI", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("          BOOTIN", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("         BOOTING", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("       BOOTING U", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("      BOOTING UP", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("     BOOTING UP ", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("    BOOTING UP R", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("   BOOTING UP RA", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("  BOOTING UP RAS", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string(" BOOTING UP RASP", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("BOOTING UP RASPB", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("OOTING UP RASPBE", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("OTING UP RASPBER", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("TING UP RASPBERR", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("ING UP RASPBERRY", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("NG UP RASPBERRY ", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("G UP RASPBERRY P", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string(" UP RASPBERRY PI", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("UP RASPBERRY PI.", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("P RASPBERRY PI..", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string(" RASPBERRY PI...", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("RASPBERRY PI... ", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("ASPBERRY PI...  ", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("SPBERRY PI...   ", 2)
+sleep(0.32) # 0.30.25 sec delay
+mylcd.lcd_display_string("      DONE      ", 2)
+
+sleep(2.5) # 2.5 sec delay
+
+mylcd.lcd_display_string("              S", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("             ST", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("            STA", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("           STAR", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("          START", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("         STARTI", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("        STARTIN", 2)
+sleep(0.25) # 0.25 sec delay 
+mylcd.lcd_display_string("       STARTING", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("      STARTING ", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("     STARTING N", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("    STARTING NA", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("   STARTING NAS", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("  STARTING NAS ", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string(" STARTING NAS S", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("STARTING NAS SE", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("TARTING NAS SER", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("ARTING NAS SERV", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("RTING NAS SERVI", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("TING NAS SERVIC", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("ING NAS SERVICE", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("NG NAS SERVICES", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("G NAS SERVICES.", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string(" NAS SERVICES..", 2)
+sleep(0.25) # 0.25 sec delay
+mylcd.lcd_display_string("NAS SERVICES...", 2)
+sleep(0.32) # 0.32 sec delay
+
+
+
+
+
+
